b41a8386e34fe8cabb80f382d11bb99123ebabb1
ghwns84 <ghwns84@gmail.com>
2020-04-22 17:43:34 +0900

    dockertest

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# useradd  $USER
+# echo -e "$PASSWD\n$PASSWD" | (passwd --stdin $USER)
+# echo  $USER password: $PASSWD
+# /bin/bash
+
+apt update -y
+apt install -y net-tools
+apt install -y curl
+apt install -y tree
---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+service nginx start
+
+echo "<h1>hi</h1>" > ~/df/zz/dockertest/index.html
